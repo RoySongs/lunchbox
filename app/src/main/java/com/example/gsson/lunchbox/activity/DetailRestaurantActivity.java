a4f7cb98cbed372ff1df6b740db86a9769727887
kimgaram0718 <kimgaram0718@naver.com>
2019-02-26 21:29:11 +0900

    음식점 상세화면 일부 기능 및 화면구성 수정

--- app/src/main/java/com/example/gsson/lunchbox/activity/DetailRestaurantActivity.java
+++ app/src/main/java/com/example/gsson/lunchbox/activity/DetailRestaurantActivity.java
@@ -1,6 +1,8 @@
 package com.example.gsson.lunchbox.activity;
 
+import android.content.Intent;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.gsson.lunchbox.R;
 import com.example.gsson.lunchbox.base.AbstBaseActivity;
@@ -9,10 +11,16 @@ import com.example.gsson.lunchbox.commons.Common;
 //가람담당_식당상세화면
 public class DetailRestaurantActivity extends AbstBaseActivity {
 
+    TextView tvMenuName;
+
     @Override
     protected void onCreateChild() {
         setContentView(R.layout.activity_detail_restaurant);
         Common.showLogD("test log debug");
+        tvMenuName=(TextView)findViewById(R.id.tvMenuName);
+
+        //음식점 목록 중 하나를 누르면 목록의 데이터를 intent에 담아 이 화면으로 이동_이 화면에서 해당 intent 값 적용
+//        getIntentData();
     }
 
     @Override
@@ -26,8 +34,23 @@ public class DetailRestaurantActivity extends AbstBaseActivity {
             case R.id.llBack:
                 showToastShort("test toast");
                 break;
-                default:
-                    break;
+            default:
+                break;
+        }
+    }
+
+    private void getIntentData() {
+        Intent i = getIntent();
+        String menuName = i.getStringExtra("menuName");
+        int distance = i.getIntExtra("distance", -1);
+
+        boolean isException = false;
+        isException = menuName == null || distance == -1;
+
+        if (isException) {
+            Common.showLogD("화면이동 후 값 받을 때 값이 제대로 안 넘어 옴_isException=" + isException);
+        } else {
+            tvMenuName.setText(menuName);
         }
     }
 }
