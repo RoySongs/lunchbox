465dd546f8c6ad46dab271e36862eda5c9739407
kimgaram0718 <kimgaram0718@naver.com>
2019-02-26 20:35:13 +0900

    garam push test

--- app/src/main/java/com/example/gsson/lunchbox/MainActivity.java
+++ app/src/main/java/com/example/gsson/lunchbox/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.gsson.lunchbox;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //garam test
+        Toast.makeText(this,"garam", Toast.LENGTH_LONG).show();
     }
 }
