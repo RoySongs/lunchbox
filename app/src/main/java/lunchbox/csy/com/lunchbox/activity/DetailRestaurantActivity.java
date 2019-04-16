75118b1b246a6bf6fc752c0d4a577648b6bfec36
kimgaram0718 <kimgaram0718@naver.com>
2019-04-16 22:26:12 +0900

    구글맵에 가짜데이터 마커 표시 기능 구현

--- app/src/main/java/lunchbox/csy/com/lunchbox/activity/DetailRestaurantActivity.java
+++ app/src/main/java/lunchbox/csy/com/lunchbox/activity/DetailRestaurantActivity.java
@@ -1,33 +1,56 @@
 package lunchbox.csy.com.lunchbox.activity;
 
+import android.app.Activity;
 import android.app.FragmentManager;
+import android.app.VoiceInteractor;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.NumberFormat;
+import java.util.ArrayList;
 
 import lunchbox.csy.com.lunchbox.R;
 import lunchbox.csy.com.lunchbox.base.AbstBaseActivity;
 import lunchbox.csy.com.lunchbox.commons.Common;
 import lunchbox.csy.com.lunchbox.commons.Const;
+import lunchbox.csy.com.lunchbox.googleMap.MarkerItem;
 
 //가람담당_식당상세화면
-public class DetailRestaurantActivity extends AbstBaseActivity implements OnMapReadyCallback {
+public class DetailRestaurantActivity extends AbstBaseActivity implements OnMapReadyCallback,
+        GoogleMap.OnMarkerClickListener, GoogleMap.OnMapClickListener {
 
-    TextView tvRestName, tvDistance;
+    private TextView tvRestName, tvDistance;
+
+    //====구글맵 관련----
+    private Marker selectedMarker;
+    private View marker_root_view;
+    private TextView tvMarker;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreateChild() {
@@ -61,7 +84,11 @@ public class DetailRestaurantActivity extends AbstBaseActivity implements OnMapR
         FragmentManager fragmentManager = getFragmentManager();
         MapFragment mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.map);
 
+//        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager()
+//        .findFragmentById(R.id.map);
+
         mapFragment.getMapAsync(this);
+
     }
 
     @Override
@@ -187,15 +214,122 @@ public class DetailRestaurantActivity extends AbstBaseActivity implements OnMapR
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        LatLng location = new LatLng(37.56, 126.97);
+//        LatLng location = new LatLng(37.56, 126.97);
+//
+//        MarkerOptions markerOptions = new MarkerOptions();
+//        markerOptions.position(location);
+//        markerOptions.title("서울");
+//        markerOptions.snippet("한국의 수도");
+//        googleMap.addMarker(markerOptions);
+//
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+//        googleMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+
+        //=====
+        mMap=googleMap;
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.537523, 126.96558), 14));
+        mMap.setOnMarkerClickListener(this);
+        mMap.setOnMapClickListener(this);
+
+        setCustumMarkerView();
+        getSampleMarkerItems();
+    }
+
+    //====구글맵 관련----
+    private void setCustumMarkerView() {
+        marker_root_view = LayoutInflater.from(this).inflate(R.layout.googlemap_marker_layout, null);
+        tvMarker = (TextView)marker_root_view.findViewById(R.id.tvMarker);
+    }
+
+    private void getSampleMarkerItems() {
+        ArrayList<MarkerItem> sampleList = new ArrayList<MarkerItem>();
+
+        sampleList.add(new MarkerItem(37.538523, 126.96568, 2500000));
+        sampleList.add(new MarkerItem(37.527523, 126.96568, 100000));
+        sampleList.add(new MarkerItem(37.549523, 126.96568, 15000));
+        sampleList.add(new MarkerItem(37.538523, 126.95768, 5000));
+
+        for(MarkerItem markerItem : sampleList) {
+            addMarker(markerItem, false);
+        }
+    }
+
+    private Marker addMarker(MarkerItem markerItem, boolean isSelectedMarker) {
+        LatLng position = new LatLng(markerItem.getLat(), markerItem.getLon());
+        int price = markerItem.getPrice();
+        String formatted = NumberFormat.getCurrencyInstance().format(price);
+
+        tvMarker.setText(formatted);
+
+        if(isSelectedMarker) {
+            tvMarker.setBackgroundResource(R.drawable.ic_launcher_background);
+            tvMarker.setTextColor(Color.WHITE);
+        } else {
+            tvMarker.setBackgroundResource(R.drawable.ic_kakao);
+            tvMarker.setTextColor(Color.BLACK);
+        }
 
         MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(location);
-        markerOptions.title("서울");
-        markerOptions.snippet("한국의 수도");
-        googleMap.addMarker(markerOptions);
+        markerOptions.title(Integer.toString(price));
+        markerOptions.position(position);
+        markerOptions.icon(BitmapDescriptorFactory.fromBitmap(createDrawableFromView(this, marker_root_view)));
 
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(location));
-        googleMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+        return mMap.addMarker(markerOptions);
+    }
+
+    private Bitmap createDrawableFromView(Context context, View view) {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity)context).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        view.measure(displayMetrics.widthPixels, displayMetrics.heightPixels);
+        view.layout(0,0,displayMetrics.widthPixels, displayMetrics.heightPixels);
+        view.buildDrawingCache();
+        Bitmap bitmap = Bitmap.createBitmap(view.getMeasuredWidth(), view.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+
+        Canvas canvas=new Canvas(bitmap);
+        view.draw(canvas);
+
+        return bitmap;
+    }
+
+    private Marker addMarker(Marker marker, boolean isSelectedMarker) {
+        double lat = marker.getPosition().latitude;
+        double lon = marker.getPosition().longitude;
+        int price = Integer.parseInt(marker.getTitle());
+        MarkerItem temp = new MarkerItem(lat, lon, price);
+
+        return addMarker(temp, isSelectedMarker);
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        CameraUpdate center = CameraUpdateFactory.newLatLng(marker.getPosition());
+        mMap.animateCamera(center);
+
+        changeSelectedMarker(marker);
+
+        return true;
+    }
+
+    private void changeSelectedMarker(Marker marker) {
+        //selected marker return
+        if(selectedMarker != null) {
+            addMarker(selectedMarker, false);
+            selectedMarker.remove();
+        }
+
+        //selected marker show
+        if(marker != null) {
+            selectedMarker = addMarker(marker, true);
+            marker.remove();
+        }
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        changeSelectedMarker(null);
     }
 }
