c4fb1f4e60b3c53ca546fff38d0fbd17cd58eec9
RoySon <sgsn2015@gmail.com>
2019-04-23 01:08:05 +0900

    푸드룰렛 수정(성지연님)

--- app/src/main/java/lunchbox/csy/com/lunchbox/roulette/FoodRoulette.java
+++ app/src/main/java/lunchbox/csy/com/lunchbox/roulette/FoodRoulette.java
@@ -1,17 +1,22 @@
 package lunchbox.csy.com.lunchbox.roulette;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.azoft.carousellayoutmanager.CarouselLayoutManager;
 import com.azoft.carousellayoutmanager.CarouselZoomPostLayoutListener;
 import com.azoft.carousellayoutmanager.CenterScrollListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import lunchbox.csy.com.lunchbox.MainActivity;
 import lunchbox.csy.com.lunchbox.R;
@@ -22,6 +27,7 @@ public class FoodRoulette extends AppCompatActivity {
     private ArrayList<Integer> images;
     private GalleryAdapter mAdapter;
     private RecyclerView recyclerView;
+    private AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +43,9 @@ public class FoodRoulette extends AppCompatActivity {
         mAdapter = new GalleryAdapter(getApplicationContext(), images);
 
 
+
         recyclerView.setAdapter(mAdapter);
+
 
         recyclerView.addOnScrollListener(new CenterScrollListener());
 
@@ -88,6 +96,24 @@ public class FoodRoulette extends AppCompatActivity {
 
 
 
+        final int speedScroll = 100;
+        final int stopTime = 10000;
+        final Handler handler = new Handler();
+
+        //롤링 시작
+        final Runnable runnable = new Runnable() {
+            int count = 0;
+            boolean flag = true;
+            @Override
+            public void run() {
+                recyclerView.smoothScrollToPosition(count);
+                count++;
+                handler.postDelayed(this,speedScroll);
+            }
+        };
+        handler.postDelayed(runnable,speedScroll);
+
+
 
 
 
@@ -96,23 +122,6 @@ public class FoodRoulette extends AppCompatActivity {
         buttonRed.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final int speedScroll = 10;
-                final int stopTime = 1000;
-                final Handler handler = new Handler();
-
-                //롤링 시작
-                final Runnable runnable = new Runnable() {
-                    int count = 0;
-                    boolean flag = true;
-                    @Override
-                    public void run() {
-                        recyclerView.smoothScrollToPosition(count);
-                        count++;
-                        handler.postDelayed(this,speedScroll);
-                    }
-                };
-                handler.postDelayed(runnable,speedScroll);
-
                 //롤링 멈추기
                 final Runnable stop_runnable = new Runnable() {
                     @Override
@@ -122,8 +131,59 @@ public class FoodRoulette extends AppCompatActivity {
                         recyclerView.smoothScrollToPosition(randomPosition);
                     }
                 };
-                handler.postDelayed(stop_runnable,stopTime);
+                handler.postDelayed(stop_runnable,0);
+
+
+
+
+
+                final Runnable dialogdelay = new Runnable() {
+                    @Override
+                    public void run() {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(FoodRoulette.this);
+                        //builder.setIcon(R.drawable.salad);
+                        builder.setTitle("당첨");
+                        builder.setMessage("메뉴 이름이 나와야하는자리");
+                        builder.setPositiveButton("공유하기", dialogListener);
+                        builder.setNegativeButton("다시하기", dialogListener);
+                        builder.setCancelable(false);
+
+
+                        alertDialog = builder.create();
+                        alertDialog.show();
+                    }
+
+                };
+                handler.postDelayed(dialogdelay, 1000);
+
+
+
             }
+
+
+
+
+
+
+
+            DialogInterface.OnClickListener dialogListener=new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    if(dialog==alertDialog && which==DialogInterface.BUTTON_POSITIVE){
+                        Intent msg = new Intent(getIntent().ACTION_SEND);
+                        msg.addCategory(Intent.CATEGORY_DEFAULT);
+                        msg.putExtra(Intent.EXTRA_SUBJECT,"런치RUN!");
+                        msg.putExtra(Intent.EXTRA_TEXT, "♥오늘의 점심메뉴는 오리구이 입니다♥");
+                        msg.putExtra(Intent.EXTRA_TITLE, "제목");
+                        msg.setType("text/plain");
+                        startActivity(Intent.createChooser(msg, "공유하기"));
+
+
+                    }else if(dialog==alertDialog && which==DialogInterface.BUTTON_NEGATIVE){
+                        runnable.run();
+                    }
+                }
+            };
         }) ;
 
 
